5960cbf7d6d710598ffe6ef84be1a6fed4acab61
Andrés Mise Olivera <j4m0@outlook.com>
2016-02-01 21:30:40 -0500

    Se agregan foreig key

--- src/main/java/co/edu/unal/sam/physicalactivity/model/domain/PreselectionResponsePreselection.java
+++ src/main/java/co/edu/unal/sam/physicalactivity/model/domain/PreselectionResponsePreselection.java
@@ -1,6 +1,7 @@
 package co.edu.unal.sam.physicalactivity.model.domain;
 
 import javax.persistence.Column;
+import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
@@ -11,19 +12,45 @@ import co.edu.unal.sam.aspect.model.domain.Entity;
 @javax.persistence.Entity
 @javax.persistence.Table(name = "preselection_response_preselection")
 public class PreselectionResponsePreselection extends Entity {
-    
-	@Column(name = "response", nullable = false, length = 300)
-	@Null
-	private String response;
 
-	@ManyToOne()
-	@JoinColumn(name = "response_preselection_id", nullable = false)
-	@NotNull
-	private ResponsePreselection responsePreselection;
-	
-	@ManyToOne()
-	@JoinColumn(name = "preselection_id", nullable = false)
-	@NotNull
-	private Preselection preselection;
+    @Column(name = "response", nullable = false, length = 300)
+    @Null
+    private String response;
+
+    @ManyToOne()
+    @JoinColumn(name = "response_preselection_id", nullable = false, foreignKey = @ForeignKey(
+            name = "fk_preselection_response_preselection_response_preselection") )
+    @NotNull
+    private ResponsePreselection responsePreselection;
+
+    @ManyToOne()
+    @JoinColumn(name = "preselection_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_preselection_response_preselection_preselection") )
+    @NotNull
+    private Preselection preselection;
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public ResponsePreselection getResponsePreselection() {
+        return responsePreselection;
+    }
+
+    public void setResponsePreselection(ResponsePreselection responsePreselection) {
+        this.responsePreselection = responsePreselection;
+    }
+
+    public Preselection getPreselection() {
+        return preselection;
+    }
+
+    public void setPreselection(Preselection preselection) {
+        this.preselection = preselection;
+    }
 
 }
--- src/main/java/co/edu/unal/sam/physicalactivity/model/domain/ResponsePreselection.java
+++ src/main/java/co/edu/unal/sam/physicalactivity/model/domain/ResponsePreselection.java
@@ -1,6 +1,7 @@
 package co.edu.unal.sam.physicalactivity.model.domain;
 
 import javax.persistence.Column;
+import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
@@ -15,25 +16,66 @@ import co.edu.unal.sam.physicalactivity.model.enumerator.TypeQuestionEnum;
 @javax.persistence.Table(name = "response_preselection")
 public class ResponsePreselection extends Entity {
 
-	@Column(name = "description", columnDefinition = "TEXT", nullable = true)
-	@Null
-	private String description;
+    @Column(name = "description", columnDefinition = "TEXT", nullable = true)
+    @Null
+    private String description;
 
-	@Column(name = "position", nullable = true)
-	@Null
-	private Boolean position;
+    @Column(name = "position", nullable = true)
+    @Null
+    private Boolean position;
 
-	@Column(name = "score", nullable = false)
-	@NotNull
-	private Integer score;
+    @Column(name = "score", nullable = false)
+    @NotNull
+    private Integer score;
 
-	@Column(name = "type_question_id")
-	@Type(type = "co.edu.unal.sam.physicalactivity.model.usertype.TypeQuestionUserType")
-	private TypeQuestionEnum typeQuestion;
+    @Column(name = "type_question_id")
+    @Type(type = "co.edu.unal.sam.physicalactivity.model.usertype.TypeQuestionUserType")
+    private TypeQuestionEnum typeQuestion;
 
-	@ManyToOne()
-	@JoinColumn(name = "question_preselection_id", nullable = false)
-	@NotNull
-	private QuestionPreselection questionPreselection;
+    @ManyToOne()
+    @JoinColumn(name = "question_preselection_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_response_preselection_question_preselection") )
+    @NotNull
+    private QuestionPreselection questionPreselection;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getPosition() {
+        return position;
+    }
+
+    public void setPosition(Boolean position) {
+        this.position = position;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public TypeQuestionEnum getTypeQuestion() {
+        return typeQuestion;
+    }
+
+    public void setTypeQuestion(TypeQuestionEnum typeQuestion) {
+        this.typeQuestion = typeQuestion;
+    }
+
+    public QuestionPreselection getQuestionPreselection() {
+        return questionPreselection;
+    }
+
+    public void setQuestionPreselection(QuestionPreselection questionPreselection) {
+        this.questionPreselection = questionPreselection;
+    }
 
 }
