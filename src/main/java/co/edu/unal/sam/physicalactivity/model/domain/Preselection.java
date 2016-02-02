62840acadc5cb46fa9d1a556a8a3efd65ee54aea
Andrés Mise Olivera <j4m0@outlook.com>
2016-02-01 20:50:27 -0500

    Se cambia nombre de propiedad

--- src/main/java/co/edu/unal/sam/physicalactivity/model/domain/Preselection.java
+++ src/main/java/co/edu/unal/sam/physicalactivity/model/domain/Preselection.java
@@ -7,7 +7,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 
 import co.edu.unal.sam.aspect.model.domain.Entity;
-import co.edu.unal.sam.physicalactivity.model.domain.*;
 
 @javax.persistence.Entity
 @javax.persistence.Table(name = "preselection")
@@ -15,7 +14,7 @@ public class Preselection extends Entity {
 
 	@Column(name = "result", nullable = true, length = 300)
 	@Null
-	private String name;
+	private String result;
 
 	@ManyToOne()
 	@JoinColumn(name = "user_id", nullable = false)
@@ -26,4 +25,31 @@ public class Preselection extends Entity {
 	@JoinColumn(name = "risk_plan_id", nullable = false)
 	@NotNull
 	private RiskPlan riskPlan;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public RiskPlan getRiskPlan() {
+        return riskPlan;
+    }
+
+    public void setRiskPlan(RiskPlan riskPlan) {
+        this.riskPlan = riskPlan;
+    }
+	
+	
+	
 }
