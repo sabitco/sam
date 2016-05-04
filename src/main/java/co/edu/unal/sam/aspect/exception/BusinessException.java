bd04f08c2641cbdc366a312de305e88b1e90a156
Andrés Mise Olivera <j4m0@outlook.com>
2016-05-04 10:50:43 -0500

    Se controla cuando no se establece la facultad

---
+++ src/main/java/co/edu/unal/sam/aspect/exception/BusinessException.java
@@ -0,0 +1,21 @@
+package co.edu.unal.sam.aspect.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BusinessException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public BusinessException() {}
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
