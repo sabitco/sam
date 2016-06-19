773330f95673b5ca7ebdb76053d74fb8af002f29
Andrés Mise Olivera <j4m0@outlook.com>
2016-06-18 20:25:16 -0500

    Se crean servicios complementarios

--- src/main/java/co/edu/unal/sam/physicalactivity/model/domain/Tip.java
+++ src/main/java/co/edu/unal/sam/physicalactivity/model/domain/Tip.java
@@ -1,11 +1,6 @@
 package co.edu.unal.sam.physicalactivity.model.domain;
 
 import javax.persistence.Column;
-import javax.persistence.ForeignKey;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 
 import co.edu.unal.sam.aspect.model.domain.Entity;
 
@@ -13,100 +8,21 @@ import co.edu.unal.sam.aspect.model.domain.Entity;
 @javax.persistence.Table(name = "tip")
 public class Tip extends Entity {
 
-	@Column(name = "body", columnDefinition = "TEXT", nullable = false)
-	@NotNull
-	private String body;
+    @Column(name = "description", columnDefinition = "TEXT", nullable = true)
+    private String description;
 
-	@Column(name = "description", columnDefinition = "TEXT", nullable = true)
-	@Null
-	private String description;
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
 
-	@ManyToOne()
-	@JoinColumn(name = "question_health_id", nullable = false, foreignKey = @ForeignKey(name = "fk_tip_question_health"))
-	@NotNull
-	private QuestionHealth questionHealth;
-
-	@Column(name = "message_alert", nullable = false, length = 300)
-	@NotNull
-	private String messageAlert;
-
-	@Column(name = "tag", nullable = false, length = 300)
-	@NotNull
-	private String tag;
-
-	/**
-	 * @return the body
-	 */
-	public String getBody() {
-		return body;
-	}
-
-	/**
-	 * @param body
-	 *            the body to set
-	 */
-	public void setBody(String body) {
-		this.body = body;
-	}
-
-	/**
-	 * @return the description
-	 */
-	public String getDescription() {
-		return description;
-	}
-
-	/**
-	 * @param description
-	 *            the description to set
-	 */
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	/**
-	 * @return the questionHealth
-	 */
-	public QuestionHealth getQuestionHealth() {
-		return questionHealth;
-	}
-
-	/**
-	 * @param questionHealth
-	 *            the questionHealth to set
-	 */
-	public void setQuestionHealth(QuestionHealth questionHealth) {
-		this.questionHealth = questionHealth;
-	}
-
-	/**
-	 * @return the messageAlert
-	 */
-	public String getMessageAlert() {
-		return messageAlert;
-	}
-
-	/**
-	 * @param messageAlert
-	 *            the messageAlert to set
-	 */
-	public void setMessageAlert(String messageAlert) {
-		this.messageAlert = messageAlert;
-	}
-
-	/**
-	 * @return the tag
-	 */
-	public String getTag() {
-		return tag;
-	}
-
-	/**
-	 * @param tag
-	 *            the tag to set
-	 */
-	public void setTag(String tag) {
-		this.tag = tag;
-	}
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
 }
