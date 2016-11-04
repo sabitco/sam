c93139d8795089c9ad98a9e02d3fc7ccaec1ecc0
fcaptuayo <captuayonovoafredy@gmail.com>
2016-11-03 22:10:02 -0500

    se adiciona ajuste a setProgress

--- src/main/java/co/edu/unal/sam/physicalactivity/model/dto/UserDto.java
+++ src/main/java/co/edu/unal/sam/physicalactivity/model/dto/UserDto.java
@@ -21,6 +21,9 @@ public class UserDto extends Dto {
     private Set<ActivityDto> activities;
     private int age;
     private BmiCategoryEnum bmi;
+    private City cityBirth;
+    private City cityResidential;
+    private String contact;
     private Date dateBirth;
     private Date dateExpireClasification;
     private Date dateIngress;
@@ -28,28 +31,25 @@ public class UserDto extends Dto {
     private String descriptionHistory;
     private Set<DiseaseDto> diseases;
     private String email;
+    private String eps;
     private Faculty faculty;
     private Set<GoalDto> goals;
     private Float height;
     private Boolean history;
     private String identityDocument;
+    private MaritalStatusEnum maritalStatus;
+    private String msisdn;
     private String password;
     private Integer progress;
     private RewardEnum reward;
     private TypeRiskEnum risk;
+    private String skype;
     private Statistic statistic;
     private String surname;
     private TypeUserEnum typeuser;
     private Boolean useCondition;
     private String username;
     private Float weight;
-    private String skype;
-    private String eps;
-    private String msisdn;
-    private String contact;
-    private City cityBirth;
-    private City cityResidential;
-    private MaritalStatusEnum maritalStatus;
 
     public UserDto() {
         super();
@@ -58,9 +58,10 @@ public class UserDto extends Dto {
         this.goals = new HashSet<>();
     }
 
-    public UserDto(Bmi bmi, Date dateBirth, Date dateExpireClasification, Date dateRegister,
-            Date dateUpdate, String email, Float height, Long id, String identityDocument,
-            String name, StateEnum state, TypeUserEnum typeuser, String username, Float weight) {
+    public UserDto(final Bmi bmi, final Date dateBirth, final Date dateExpireClasification,
+            final Date dateRegister, final Date dateUpdate, final String email, final Float height,
+            final Long id, final String identityDocument, final String name, final StateEnum state,
+            final TypeUserEnum typeuser, final String username, final Float weight) {
         super(dateRegister, dateUpdate, id, name, state);
         this.bmi = bmi.getCategory();
         this.risk = bmi.getRisk();
@@ -73,8 +74,8 @@ public class UserDto extends Dto {
         this.dateBirth = dateBirth;
         this.weight = weight;
         this.progress = 1;
-        LocalDate start = new java.sql.Date(dateBirth.getTime()).toLocalDate();
-        LocalDate end = LocalDate.now();
+        final LocalDate start = new java.sql.Date(dateBirth.getTime()).toLocalDate();
+        final LocalDate end = LocalDate.now();
         this.age = (int) ChronoUnit.YEARS.between(start, end);
     }
 
@@ -97,6 +98,27 @@ public class UserDto extends Dto {
      */
     public BmiCategoryEnum getBmi() {
         return this.bmi;
+    }
+
+    /**
+     * @return the cityBirth
+     */
+    public City getCityBirth() {
+        return this.cityBirth;
+    }
+
+    /**
+     * @return the cityResidential
+     */
+    public City getCityResidential() {
+        return this.cityResidential;
+    }
+
+    /**
+     * @return the contact
+     */
+    public String getContact() {
+        return this.contact;
     }
 
     /**
@@ -149,6 +171,13 @@ public class UserDto extends Dto {
     }
 
     /**
+     * @return the eps
+     */
+    public String getEps() {
+        return this.eps;
+    }
+
+    /**
      * @return the faculty
      */
     public Faculty getFaculty() {
@@ -184,6 +213,20 @@ public class UserDto extends Dto {
     }
 
     /**
+     * @return the maritalStatus
+     */
+    public MaritalStatusEnum getMaritalStatus() {
+        return this.maritalStatus;
+    }
+
+    /**
+     * @return the msisdn
+     */
+    public String getMsisdn() {
+        return this.msisdn;
+    }
+
+    /**
      * @return the password
      */
     public String getPassword() {
@@ -209,6 +252,13 @@ public class UserDto extends Dto {
      */
     public TypeRiskEnum getRisk() {
         return this.risk;
+    }
+
+    /**
+     * @return the skype
+     */
+    public String getSkype() {
+        return this.skype;
     }
 
     /**
@@ -256,281 +306,226 @@ public class UserDto extends Dto {
     /**
      * @param activities the activities to set
      */
-    public void setActivities(Set<ActivityDto> activities) {
+    public void setActivities(final Set<ActivityDto> activities) {
         this.activities = activities;
     }
 
     /**
      * @param age the age to set
      */
-    public void setAge(int age) {
+    public void setAge(final int age) {
         this.age = age;
     }
 
     /**
      * @param bmi the bmi to set
      */
-    public void setBmi(BmiCategoryEnum bmi) {
+    public void setBmi(final BmiCategoryEnum bmi) {
         this.bmi = bmi;
+    }
+
+    /**
+     * @param cityBirth the cityBirth to set
+     */
+    public void setCityBirth(final City cityBirth) {
+        this.cityBirth = cityBirth;
+    }
+
+    /**
+     * @param cityResidential the cityResidential to set
+     */
+    public void setCityResidential(final City cityResidential) {
+        this.cityResidential = cityResidential;
+    }
+
+    /**
+     * @param contact the contact to set
+     */
+    public void setContact(final String contact) {
+        this.contact = contact;
     }
 
     /**
      * @param dateBirth the dateBirth to set
      */
-    public void setDateBirth(Date dateBirth) {
+    public void setDateBirth(final Date dateBirth) {
         this.dateBirth = dateBirth;
     }
 
     /**
      * @param dateExpireClasification the dateExpireClasification to set
      */
-    public void setDateExpireClasification(Date dateExpireClasification) {
+    public void setDateExpireClasification(final Date dateExpireClasification) {
         this.dateExpireClasification = dateExpireClasification;
     }
 
     /**
      * @param dateIngress the dateIngress to set
      */
-    public void setDateIngress(Date dateIngress) {
+    public void setDateIngress(final Date dateIngress) {
         this.dateIngress = dateIngress;
     }
+
 
     /**
      * @param dateInteraction the dateInteraction to set
      */
-    public void setDateInteraction(Date dateInteraction) {
+    public void setDateInteraction(final Date dateInteraction) {
         this.dateInteraction = dateInteraction;
     }
 
     /**
      * @param descriptionHistory the descriptionHistory to set
      */
-    public void setDescriptionHistory(String descriptionHistory) {
+    public void setDescriptionHistory(final String descriptionHistory) {
         this.descriptionHistory = descriptionHistory;
     }
 
     /**
      * @param diseases the diseases to set
      */
-    public void setDiseases(Set<DiseaseDto> diseases) {
+    public void setDiseases(final Set<DiseaseDto> diseases) {
         this.diseases = diseases;
     }
 
     /**
      * @param email the email to set
      */
-    public void setEmail(String email) {
+    public void setEmail(final String email) {
         this.email = email;
+    }
+
+    /**
+     * @param eps the eps to set
+     */
+    public void setEps(final String eps) {
+        this.eps = eps;
     }
 
     /**
      * @param faculty the faculty to set
      */
-    public void setFaculty(Faculty faculty) {
+    public void setFaculty(final Faculty faculty) {
         this.faculty = faculty;
     }
 
     /**
      * @param goals the goals to set
      */
-    public void setGoals(Set<GoalDto> goals) {
+    public void setGoals(final Set<GoalDto> goals) {
         this.goals = goals;
     }
 
     /**
      * @param height the height to set
      */
-    public void setHeight(Float height) {
+    public void setHeight(final Float height) {
         this.height = height;
     }
 
     /**
      * @param history the history to set
      */
-    public void setHistory(Boolean history) {
+    public void setHistory(final Boolean history) {
         this.history = history;
     }
 
     /**
      * @param identityDocument the identityDocument to set
      */
-    public void setIdentityDocument(String identityDocument) {
+    public void setIdentityDocument(final String identityDocument) {
         this.identityDocument = identityDocument;
+    }
+
+    /**
+     * @param maritalStatus the maritalStatus to set
+     */
+    public void setMaritalStatus(final MaritalStatusEnum maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    /**
+     * @param msisdn the msisdn to set
+     */
+    public void setMsisdn(final String msisdn) {
+        this.msisdn = msisdn;
     }
 
     /**
      * @param password the password to set
      */
-    public void setPassword(String password) {
+    public void setPassword(final String password) {
         this.password = password;
     }
 
     /**
      * @param progress the progress to set
      */
-    public final void setProgress(int progress) {
+    public void setProgress(final Integer progress) {
         this.progress = progress;
     }
 
     /**
      * @param reward the reward to set
      */
-    public void setReward(RewardEnum reward) {
+    public void setReward(final RewardEnum reward) {
         this.reward = reward;
     }
 
     /**
      * @param risk the risk to set
      */
-    public void setRisk(TypeRiskEnum risk) {
+    public void setRisk(final TypeRiskEnum risk) {
         this.risk = risk;
+    }
+
+    /**
+     * @param skype the skype to set
+     */
+    public void setSkype(final String skype) {
+        this.skype = skype;
     }
 
     /**
      * @param statistic the statistic to set
      */
-    public void setStatistic(Statistic statistic) {
+    public void setStatistic(final Statistic statistic) {
         this.statistic = statistic;
     }
 
     /**
      * @param surname the surname to set
      */
-    public void setSurname(String surname) {
+    public void setSurname(final String surname) {
         this.surname = surname;
     }
 
     /**
      * @param typeuser the typeuser to set
      */
-    public void setTypeuser(TypeUserEnum typeuser) {
+    public void setTypeuser(final TypeUserEnum typeuser) {
         this.typeuser = typeuser;
     }
 
     /**
      * @param useCondition the useCondition to set
      */
-    public void setUseCondition(Boolean useCondition) {
+    public void setUseCondition(final Boolean useCondition) {
         this.useCondition = useCondition;
     }
 
     /**
      * @param username the username to set
      */
-    public void setUsername(String username) {
+    public void setUsername(final String username) {
         this.username = username;
     }
 
     /**
      * @param weight the weight to set
      */
-    public void setWeight(Float weight) {
+    public void setWeight(final Float weight) {
         this.weight = weight;
-    }
-
-    /**
-     * @return the skype
-     */
-    public String getSkype() {
-        return skype;
-    }
-
-    /**
-     * @param skype the skype to set
-     */
-    public void setSkype(String skype) {
-        this.skype = skype;
-    }
-
-    /**
-     * @return the eps
-     */
-    public String getEps() {
-        return eps;
-    }
-
-    /**
-     * @param eps the eps to set
-     */
-    public void setEps(String eps) {
-        this.eps = eps;
-    }
-
-    /**
-     * @return the msisdn
-     */
-    public String getMsisdn() {
-        return msisdn;
-    }
-
-    /**
-     * @param msisdn the msisdn to set
-     */
-    public void setMsisdn(String msisdn) {
-        this.msisdn = msisdn;
-    }
-
-    /**
-     * @return the contact
-     */
-    public String getContact() {
-        return contact;
-    }
-
-    /**
-     * @param contact the contact to set
-     */
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
-
-    /**
-     * @return the cityBirth
-     */
-    public City getCityBirth() {
-        return cityBirth;
-    }
-
-    /**
-     * @param cityBirth the cityBirth to set
-     */
-    public void setCityBirth(City cityBirth) {
-        this.cityBirth = cityBirth;
-    }
-
-    /**
-     * @return the cityResidential
-     */
-    public City getCityResidential() {
-        return cityResidential;
-    }
-
-    /**
-     * @param cityResidential the cityResidential to set
-     */
-    public void setCityResidential(City cityResidential) {
-        this.cityResidential = cityResidential;
-    }
-
-    /**
-     * @return the maritalStatus
-     */
-    public MaritalStatusEnum getMaritalStatus() {
-        return maritalStatus;
-    }
-
-    /**
-     * @param maritalStatus the maritalStatus to set
-     */
-    public void setMaritalStatus(MaritalStatusEnum maritalStatus) {
-        this.maritalStatus = maritalStatus;
-    }
-
-    /**
-     * @param progress the progress to set
-     */
-    public void setProgress(Integer progress) {
-        this.progress = progress;
     }
 
 }
